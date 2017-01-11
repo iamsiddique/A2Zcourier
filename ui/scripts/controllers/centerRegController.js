edd9af320907c0706654b4f07a3d98e35daf2ad1
itsmesiddique22 <aboobacker.ali@corp.broadsoft.com>
2017-01-11 21:52:54 +0530

    Client requested fix in center of registration is done

--- ui/scripts/controllers/centerRegController.js
+++ ui/scripts/controllers/centerRegController.js
@@ -4,7 +4,7 @@ courierApp.controller("centerRegController",['$rootScope','$scope','$location','
  	$scope.pinCode;
  	$rootScope.loginPage=true;
 	$scope.addArea = function (){
-		
+		$scope.streetAddress;
 		intermediateService.getArea($scope.pinCode, function(response) {
 			if(response.ResponseCode == 0){
 				var centers = response.Data;
@@ -14,10 +14,11 @@ courierApp.controller("centerRegController",['$rootScope','$scope','$location','
 				pinDetails.city = centers[0].City;
 				pinDetails.country = centers[0].Country;
 				pinDetails.state = centers[0].State;
-				for(i in centers){
+				/*for(i in centers){
 					myarray.push(centers[i].Address);
-				}
-				pinDetails.address = myarray.toString();
+				}*/
+				//pinDetails.address = myarray.toString();
+				pinDetails.address = $scope.streetAddress;
 				console.log(pinDetails);
 				intermediateService.postArea(pinDetails, function(response) {
 				if(response.statusCode == 1){
