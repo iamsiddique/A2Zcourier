f25b8d6ceb29c81ae7e52f3ecad65d59c49d3f1a
iamsiddique <emailtosiddique@gmail.com>
2018-05-17 22:52:12 +0530

    changes

--- ui/scripts/controllers/stockDispatchController.js
+++ ui/scripts/controllers/stockDispatchController.js
@@ -219,7 +219,62 @@ courierApp.controller("invoiceController", ['$rootScope', '$scope', '$location',
     function ($rootScope, $scope, $location, intermediateService, $timeout, $filter) {
 
         $scope.invdata = intermediateService.invoiceDetails;
+        $scope.invdata.couriername = intermediateService.couriername;
         $scope.prd = intermediateService.dummy
+
+        $scope.demoFromHTML= function() {
+            var pdf = new jsPDF('p', 'pt', 'letter');
+            // source can be HTML-formatted string, or a reference
+            // to an actual DOM element from which the text will be scraped.
+            source = $('#idone')[0];
+    
+            // we support special element handlers. Register them with jQuery-style 
+            // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+            // There is no support for any other type of selectors 
+            // (class, of compound) at this time.
+            specialElementHandlers = {
+                // element with id of "bypass" - jQuery style selector
+                '#bypassme': function (element, renderer) {
+                    // true = "handled elsewhere, bypass text extraction"
+                    return true
+                }
+            };
+            margins = {
+                top: 80,
+                bottom: 60,
+                left: 40,
+                width: 522
+            };
+            // all coords and widths are in jsPDF instance's declared units
+            // 'inches' in this case
+            pdf.fromHTML(
+            source, // HTML string or DOM elem ref.
+            margins.left, // x coord
+            margins.top, { // y coord
+                'width': margins.width, // max width of content on PDF
+                'elementHandlers': specialElementHandlers
+            },
+    
+            function (dispose) {
+                // dispose: object with X, Y of the last line add to the PDF 
+                //          this allow the insertion of new lines after html
+                pdf.save('Test.pdf');
+            }, margins);
+        }
+        $scope.export = function(){
+            html2canvas(document.getElementById('idone'), {
+                onrendered: function (canvas) {
+                    var data = canvas.toDataURL();
+                    var docDefinition = {
+                        content: [{
+                            image: data,
+                            width: 500,
+                        }]
+                    };
+                    pdfMake.createPdf(docDefinition).download("test.pdf");
+                }
+            });
+        }
 
     }
 ]);
